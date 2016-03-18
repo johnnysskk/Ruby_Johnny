0db4c742a6690152acc94fb83e77a2db1e281f99
Johnny <johnnysskk@gmail.com>
2016-03-17 20:18:55 -0700

    code challenge for assesment

---
+++ code_challege/atbash.rb
@@ -0,0 +1,27 @@
+class Atbash
+  #build the table char part
+  @@MAP_TABLE = {}
+  small_char_array = ('a'..'z').to_a.reverse
+  count = 0
+  ('a'..'z').each do |char|
+    @@MAP_TABLE[char] = small_char_array[count]
+    @@MAP_TABLE[char.upcase] = small_char_array[count]
+    count += 1
+  end
+  #build the table number part 
+  (0..9).each do |num|
+    @@MAP_TABLE[num.to_s] = num.to_s
+  end
+
+  def self.encode(string)
+    string_array = string.delete(' ,.').split("")
+    new_array = []
+    string_array.each do |x|
+      new_array << @@MAP_TABLE[x]
+    end
+    new_array.join
+             .scan(/.....|.+/)
+             .join(' ')
+  end
+  
+end
